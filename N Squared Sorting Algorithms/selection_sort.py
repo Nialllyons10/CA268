b6bdbde2b37ef160035e8a77d7e05b37e0ac552f
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 13:14:42 +0000

    Create selection_sort.py

---
+++ N Squared Sorting Algorithms/selection_sort.py
@@ -0,0 +1,20 @@
+ # """ Selection sort algorithm """
+ #
+ 
+ def selection_sort(lst):
+    compares = 0
+    moves = 0
+    
+    for i in range(len(lst) - 1):
+       # Find the smallest item in the lst starting at i
+       min_index = i
+       for j in range(min_index + 1, len(lst)):
+           compares += 1
+          if lst[j] < lst[min_index]:
+              min_index = j
+      
+      # place smallest at the beginning (swap min index with i)
+      lst[i], lst[min_index] = lst[min_index], lst[i]
+      moves += 3
+      
+  return compares, moves
