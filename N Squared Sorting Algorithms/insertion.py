dd9d4d7afcb844084e50cdb24334d1129c9c2409
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 13:06:51 +0000

    Create insertion.py

---
+++ N Squared Sorting Algorithms/insertion.py
@@ -0,0 +1,13 @@
+#
+#   Return an empty list to get your list of numbers.
+#
+#   Then return a list of lists corresponding to the passes of an insertion sort on the numbers.
+#
+def solution():
+   return [
+      [11, 17],
+      [8, 11, 17], 
+      [8, 11, 13, 17],
+      [6, 8 , 11, 13, 17], 
+      [6, 8 , 11, 13, 17, 19]
+      ]
