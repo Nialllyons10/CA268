1f98387f1f9de1f46a6fff563255a4d4539c5510
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-23 12:43:47 +0000

    Create duplicates.py

---
+++ Linked Lists/duplicates.py
@@ -0,0 +1,43 @@
+#
+#  Just a class to store the item and the next pointer
+#
+class Node:
+  def __init__(self, item, next):
+  self.item = item
+  self.next = next
+
+# Note, these are methods "A method is a function that is stored as a class attribute"
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+  def add(self, item):
+    self.head = Node(item, self.head)
+
+  def remove(self):
+    if self.is_empty():
+      return None
+    else:
+      item = self.head.item
+      self.head = self.head.next    # remove the item by moving the head pointer
+    return item
+
+  def is_empty(self):
+    return self.head == None
+
+  def count(self):
+    return self.recursive_count(self.head)
+
+  def duplicates(self):
+    return self.check(self.head)
+      
+  def check(self,ptr):
+    if ptr==None:
+        return False
+    elif ptr.next==None:
+        return False
+    
+    if ptr.item != ptr.next.item:
+    	return self.check(ptr.next)  
+    else:
+    	return True
