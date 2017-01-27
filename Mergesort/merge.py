9fd3e8cd782206f29f5f43899f5c79f0dbfa2f82
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-01-27 12:31:37 +0000

    Rename split.py to merge.py

---
+++ Mergesort/merge.py
@@ -0,0 +1,21 @@
+""" this function will merge q1 and q2 into q.
+       Assuming that q1 and q2 are sorted, this function will
+       return q such that q contains the combined elements of q1 and q2 and
+     q will also be sorted.
+       
+       The function returns nothing. The result will be contained in the queue parameter.
+  """
+from Queue import Queue
+
+def merge(q1, q2, q):
+  while not q1.isempty() and not q2.isempty():
+      if q1.first() < q2.first():
+          q.enqueue(q1.dequeue())
+      else:
+          q.enqueue(q2.dequeue())
+  
+  while not q1.isempty():
+       q.enqueue(q1.dequeue())
+  
+  while not q2.isempty():
+       q.enqueue(q2.dequeue())
